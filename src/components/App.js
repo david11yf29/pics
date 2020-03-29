5b89592ec41b9c99aa11633e8fafa806abd20293
david11yf29 <david11yf29@gmail.com>
2020-03-29 11:45:35 +0800

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos',{
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 334312b1ace7d83eecd4d66794f99e6a73ffa6ed58b3c0eb23d4620f8c332ad7'
+            }
+        })
     }
 
     render() {
