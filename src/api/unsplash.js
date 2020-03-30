ead217f6694fb366bcfa961ce12a84b0e3ea07d0
david11yf29 <david11yf29@gmail.com>
2020-03-30 12:19:11 +0800

    updated

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID 334312b1ace7d83eecd4d66794f99e6a73ffa6ed58b3c0eb23d4620f8c332ad7'
-    }
-});
