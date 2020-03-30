80d0580550d8e80343974666afe2456704918817
david11yf29 <david11yf29@gmail.com>
2020-03-30 19:06:19 +0800

    updated

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const VideoItems = (props) => {
-    return <div>Video Item</div>
+const VideoItems = ({ video }) => {
+    // console.log(props);
+    return (
+        <div>
+            <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} />
+            {video.snippet.title}
+        </div>
+    )
 }
 
 export default VideoItems;
