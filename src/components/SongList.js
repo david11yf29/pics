abe07e2ff606ebc1ebc35301e4d4c8cdbfd51de8
david11yf29 <david11yf29@gmail.com>
2020-03-30 23:26:34 +0800

    updated

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,11 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends React.Component {
+
+    renderList() {
+        return this.props.songsFromMSTP.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">
+                        {song.title}
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
         // console.log(this);
         console.log(this.props);
         return (
-            <div>Song List</div>
+            <div className="ui divided list">{this.renderList()}</div>
         )
     }
 }
