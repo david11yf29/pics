64f12bd857451c2eca5a2ddc61f69d842e3a330e
david11yf29 <david11yf29@gmail.com>
2020-03-30 23:02:36 +0800

    updated

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class SongList extends React.Component {
-    // constructor() {
-    //     super();
-    // }
-
     render() {
+        // console.log(this);
+        console.log(this.props);
         return (
             <div>Song List</div>
         )
     }
 }
 
-export default SongList;
+const mapStateToProps = (state) => {
+    // console.log(state);
+    // change state to props
+    return {
+        songsFromMSTP: state.songsFromCR
+    };
+}
+
+export default connect(mapStateToProps)(SongList);
