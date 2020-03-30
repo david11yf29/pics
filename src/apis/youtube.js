cc41a731ee5bcd0055909924d3d52d83090797dc
david11yf29 <david11yf29@gmail.com>
2020-03-30 12:55:59 +0800

    updated

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
-// AIzaSyDpdv6EG5_7p1CkINBYI_2RM-T8GxaePVE
+import axios from 'axios';
+
+const KEY = 'AIzaSyDpdv6EG5_7p1CkINBYI_2RM-T8GxaePVE';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY 
+    }
+})
