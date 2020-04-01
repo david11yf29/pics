ea39ec70c1b141294d1f445de7767d198ff5bb42
david11yf29 <david11yf29@gmail.com>
2020-04-01 20:09:08 +0800

    updated

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,10 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async dispatch => {
+    dispatch(fetchPosts());
+}
+
 export const fetchPosts = () => {
     return async function(dispatch, getState) {
         const response = await jsonPlaceholder.get('/posts');
@@ -12,17 +16,13 @@ export const fetchPosts = () => {
     }
 };
 
-export const fetchUser = (id) => dispatch => {
-    _fetchUser(id, dispatch);
-}
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({
         type: 'FETCH_USERS',
         payload: response.data
     })
-})
+}
 
 // const _fetchUser = _.memoize(async (id, dispatch) => {
 //     const response = await jsonPlaceholder.get(`/users/${id}`);
