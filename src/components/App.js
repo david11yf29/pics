d9387b11214f21820915bc839446d91fd4546895
david11yf29 <david11yf29@gmail.com>
2020-04-05 09:07:14 +0800

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 
 const App = () => {
+
+    const [resource, setResource] = useState('posts');
+    
     return (
         <div>
             <div>
                 <button 
-                    onClick={() => this.setState({ resource: 'posts' })}>Posts</button>
+                    onClick={() => setResource('posts')}>Posts</button>
                 <button 
-                    onClick={() => this.setState({ resource: 'todos' })}>Todos</button>
+                    onClick={() => setResource('todos')}>Todos</button>
             </div>
-            {this.state.resource}
+            {resource}
         </div>
     )
 }
